fc81dbd526581475dcf7233c6be9f29d0c64dc0a
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-12-11 23:57:07 +0300

    create UI/Icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,25 +1,25 @@
-import Heart from "../assets/icons/Heart.svg?react";
-import Burger from "../assets/icons/Burger.svg?react";
+import Heart from "../assets/icons/heart.svg?react";
+import Burger from "../assets/icons/burger.svg?react";
 import Search from "../assets/icons/search.svg?react";
 import Cancellation from "../assets/icons/cancellation.svg?react";
 import Dislike from "../assets/icons/dislike.svg?react";
-import ArrowDown from "../assets/icons/down-arrow.svg?react";
+import Arrowdown from "../assets/icons/down-arrow.svg?react";
 import Instagram from "../assets/icons/instagram.svg?react";
-import Interaction from "../assets/icons/Interaction.svg?react";
+import Interaction from "../assets/icons/interaction.svg?react";
 import Korzina from "../assets/icons/korzina.svg?react";
 import Like from "../assets/icons/like.svg?react";
 import Star from "../assets/icons/star.svg?react";
 import Telegram from "../assets/icons/telegram.svg?react";
-import Arrow from "../assets/icons/arrow.svg?react"
+import Arrow from "../assets/icons/arrow.svg?react";
 import Telephone from "../assets/icons/telephone.svg?react";
-import Location from "../assets/icons/location.svg?react"
-import Logo from "../assets/icons/Logo.svg?react"
-import Photo from "../assets/icons/Photo.svg?react"
-import Circle from "../assets/icons/circle.svg?react"
-import ActionMenu from "../assets/icons/Action-menu.svg?react"
-import ArrowRight from "../assets/icons/Arrow-right.svg?react"
-import ArrowLeft from "../assets/icons/Arrow-left.svg?react"
-import Google from "../assets/icons/Google.svg?react"
+import Location from "../assets/icons/location.svg?react";
+import Logo from "../assets/icons/logo.svg?react";
+import Photo from "../assets/icons/photo.svg?react";
+import Circle from "../assets/icons/circle.svg?react";
+import ActionMenu from "../assets/icons/action-menu.svg?react";
+import Arrowright from "../assets/icons/arrow-right.svg?react";
+import Arrowleft from "../assets/icons/arrow-left.svg?react";
+import Google from "../assets/icons/google.svg?react";
 export const Icons = {
   Heart,
   Burger,
@@ -27,7 +27,7 @@ export const Icons = {
   Arrow,
   Cancellation,
   Dislike,
-  ArrowDown,
+  Arrowdown,
   Instagram,
   Interaction,
   Korzina,
@@ -40,7 +40,7 @@ export const Icons = {
   Photo,
   Circle,
   ActionMenu,
-  ArrowRight,
-  ArrowLeft,
-  Google
+  Arrowright,
+  Arrowleft,
+  Google,
 };
