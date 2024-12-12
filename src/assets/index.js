03b31fcba5cf5ddf660b9d709f0e383004804364
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-12-12 19:31:58 +0300

    add UI/Icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,8 +2,8 @@ import Heart from "../assets/icons/heart.svg?react";
 import Burger from "../assets/icons/burger.svg?react";
 import Search from "../assets/icons/search.svg?react";
 import Cancellation from "../assets/icons/cancellation.svg?react";
-import Dislike from "../assets/icons/dislike.svg?react";
-import Arrowdown from "../assets/icons/down-arrow.svg?react";
+import DisLike from "../assets/icons/dislike.svg?react";
+import ArrowDown from "../assets/icons/down-arrow.svg?react";
 import Instagram from "../assets/icons/instagram.svg?react";
 import Interaction from "../assets/icons/interaction.svg?react";
 import Korzina from "../assets/icons/korzina.svg?react";
@@ -17,9 +17,10 @@ import Logo from "../assets/icons/logo.svg?react";
 import Photo from "../assets/icons/photo.svg?react";
 import Circle from "../assets/icons/circle.svg?react";
 import ActionMenu from "../assets/icons/action-menu.svg?react";
-import Arrowright from "../assets/icons/arrow-right.svg?react";
-import Arrowleft from "../assets/icons/arrow-left.svg?react";
+import ArrowRight from "../assets/icons/arrow-right.svg?react";
+import ArrowLeft from "../assets/icons/arrow-left.svg?react";
 import Google from "../assets/icons/google.svg?react";
+import HeartColorless from "../assets/icons/heart-colorless.svg?react";
 
 export const Icons = {
   Heart,
@@ -27,8 +28,8 @@ export const Icons = {
   Search,
   Arrow,
   Cancellation,
-  Dislike,
-  Arrowdown,
+  DisLike,
+  ArrowDown,
   Instagram,
   Interaction,
   Korzina,
@@ -41,7 +42,8 @@ export const Icons = {
   Photo,
   Circle,
   ActionMenu,
-  Arrowright,
-  Arrowleft,
+  ArrowRight,
+  ArrowLeft,
   Google,
+  HeartColorless,
 };
