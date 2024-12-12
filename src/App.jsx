cbca6aa12446c7b792aea6cd0f8485feff39e18e
Aitula-svg <aitu.ru00@gmail.com>
2024-12-12 18:20:22 +0300

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-export const App = () => {
-  return <div></div>;
+import React from "react";
+import { Button } from "./components/UI/Button";
+import { Container, Icon, styled } from "@mui/material";
+
+const App = () => {
+  return (
+    <Container>
+      <Button variant="outlined">BUTTON</Button>
+
+      <Button variant="contained">Button</Button>
+      <Button variant="warning">BUTTON</Button>
+    </Container>
+  );
 };
+
+export default App;
