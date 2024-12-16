1631df8ab7b96c0bbeb7f9893978a04d3c442113
Bilal Gyazitdinov <gyazitdinovb@gmail.com>
2024-12-16 03:10:37 +0300

    commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,27 +1,27 @@
- import Heart from "../assets/icons/heart.svg?react"
- import Burger from "../assets/icons/burger.svg?react"
- import Search from "../assets/icons/search.svg?react"
- import Arrow from "../assets/icons/arrow.svg?react"
- import Cancellation from "../assets/icons/cancellation.svg?react"
- import DisLike from "../assets/icons/dislike.svg?react"
- import ArrowDown from "../assets/icons/down-arrow.svg?react"
- import Instagram from "../assets/icons/instagram.svg?react"
- import Interaction from "../assets/icons/interaction.svg?react"
- import Korzina from "../assets/icons/korzina.svg?react"
- import Like from "../assets/icons/like.svg?react"
- import Star from "../assets/icons/star.svg?react"
- import Telegram from "../assets/icons/telegram.svg?react"
- import Telephone from "../assets/icons/telephone.svg?react"
- import Location from "../assets/icons/location.svg?react"
- import Logo from "../assets/icons/logo.svg?react"
- import Photo from "../assets/icons/photo.svg?react"
- import Circle from "../assets/icons/circle.svg?react"
- import ActionMenu from "../assets/icons/action-menu.svg?react"
- import ArrowRight from "../assets/icons/arrow-right.svg?react"
- import ArrowLeft from "../assets/icons/arrow-left.svg?react"
- import Google from "../assets/icons/google.svg?react"
- import HeartColorless from "../assets/icons/heart-colorless.svg?react"
- 
+import Heart from "../assets/icons/heart.svg?react";
+import Burger from "../assets/icons/burger.svg?react";
+import Search from "../assets/icons/search.svg?react";
+import Arrow from "../assets/icons/arrow.svg?react";
+import Cancellation from "../assets/icons/cancellation.svg?react";
+import DisLike from "../assets/icons/dislike.svg?react";
+import ArrowDown from "../assets/icons/down-arrow.svg?react";
+import Instagram from "../assets/icons/instagram.svg?react";
+import Interaction from "../assets/icons/interaction.svg?react";
+import Korzina from "../assets/icons/korzina.svg?react";
+import Like from "../assets/icons/like.svg?react";
+import Star from "../assets/icons/star.svg?react";
+import Telegram from "../assets/icons/telegram.svg?react";
+import Telephone from "../assets/icons/telephone.svg?react";
+import Location from "../assets/icons/location.svg?react";
+import Logo from "../assets/icons/logo.svg?react";
+import Photo from "../assets/icons/photo.svg?react";
+import Circle from "../assets/icons/circle.svg?react";
+import ActionMenu from "../assets/icons/action-menu.svg?react";
+import ArrowRight from "../assets/icons/arrow-right.svg?react";
+import ArrowLeft from "../assets/icons/arrow-left.svg?react";
+import Google from "../assets/icons/google.svg?react";
+import HeartColorless from "../assets/icons/heart-colorless.svg?react";
+
 export const Icons = {
   Heart,
   Burger,
