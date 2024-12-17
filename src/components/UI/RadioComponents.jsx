9a153972c9591c4ade9cd0a73adc1363a0b02e14
Munaraaz <munmk@bk.ru>
2024-12-17 19:59:40 +0300

    radio-checkbox

---
+++ src/components/UI/RadioComponents.jsx
@@ -0,0 +1,29 @@
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { orange } from "@mui/material/colors";
+import { useState } from "react";
+
+export const RadioComponents = () => {
+  const [selectedRadio, setSelectedRadio] = useState();
+
+  const handleChangeRadio = (event) => {
+    setSelectedRadio(event.target.value);
+  };
+  return (
+    <Box>
+      <FormControl component="fieldset">
+        <RadioGroup value={selectedRadio} onChange={handleChangeRadio}>
+          <FormControlLabel
+            value="radio"
+            control={<Radio sx={{ "&.Mui-checked": { color: orange[500] } }} />}
+          />
+        </RadioGroup>
+      </FormControl>
+    </Box>
+  );
+};
