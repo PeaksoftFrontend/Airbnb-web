fdc8814379f582733e15d95f7c7cd4d545ec280a
Munaraaz <munmk@bk.ru>
2024-12-21 18:32:50 +0300

    Merge branch 'development' of https://github.com/PeaksoftFrontend/Airbnb-web into feature/checkbox

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import { App } from "./App.jsx";
 import "./index.css";
-=======
-import "./index.css";
-import { App } from "./App";
 
->>>>>>> 477cc00374c42753ca40afa5f879c786cfeabfde
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
