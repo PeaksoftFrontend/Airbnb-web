f4c6781b9e09fa0574cdb02f0b5466d7f4f8deb9
Aitula-svg <aitu.ru00@gmail.com>
2025-01-03 15:03:10 +0300

    toastify

---
+++ src/utils/helpers/showToast.js
@@ -0,0 +1,29 @@
+// ToastManager.js
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
+export const showToast = (
+  message,
+  type = "info",
+  duration = 3000,
+  options = {}
+) => {
+  const colors = {
+    info: "#007bff",
+    success: " #F0FFF1",
+    error: "rgb(226, 185, 185)",
+    warning: "#ffc107",
+  };
+
+  Toastify({
+    text: message,
+    duration,
+    close: true,
+    gravity: "top",
+    position: "right",
+    backgroundColor: colors[type] || "#000",
+    stopOnFocus: true,
+
+    ...options,
+  }).showToast();
+};
