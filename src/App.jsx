42952d9bb0392876d4ffabc6a90da1fbbec04b7a
Munaraaz <munmk@bk.ru>
2025-01-07 02:28:45 +0300

    prof

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Profile } from "./components/admin/Profile";
+
+const Users = [
+  { name: "Медер Медербеков", email: "mederbekov@gmail.com" },
+  { name: "Алексей Иванов", email: "alexey.ivanov@example.com" },
+];
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      {Users.map((user, index) => (
+        <Profile key={index} name={user.name} email={user.email} />
+      ))}
+    </div>
+  );
 };
--- src/components/admin/Profile.jsx
+++ src/components/admin/Profile.jsx
@@ -1,11 +1,13 @@
-import { Avatar, styled } from "@mui/material";
+import { Avatar, Box, styled } from "@mui/material";
 
 export const Profile = ({ fullName, name, email }) => {
   return (
     <>
       <Styledh1>{fullName}</Styledh1>
       <StyledContainer>
-        <StyledAvatar>{name.charAt(0).toUpperCase()}</StyledAvatar>
+        <AvatarBox>
+          <StyledAvatar>{name.charAt(0).toUpperCase()}</StyledAvatar>
+        </AvatarBox>
         <StyledContent>
           <StyledDiv>
             <StyledP>Name:</StyledP>
@@ -34,47 +36,52 @@ const StyledAvatar = styled(Avatar)({
   height: "89px",
   fontSize: "38px",
   backgroundColor: "#266BD3",
-  marginLeft: "141px",
-  marginTop: "38px",
+  // marginLeft: "141px",
+  // marginTop: "38px",
+});
+const AvatarBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
 });
 const StyledDiv = styled("div")({
   display: "flex",
-  marginTop: "15px",
+  flexDirection: "column",
+  // marginTop: "15px",
 });
 
 const StyledDivs = styled("div")({
   display: "flex",
   flexDirection: "column",
-  marginTop: "15px",
+  // marginTop: "15px",
 });
 
 const StyledContent = styled("div")({
   display: "flex",
   gap: "16px",
-  marginTop: "160px",
+  // marginTop: "160px",
 });
 
 const StyledP = styled("p")({
   fontSize: "16px",
   fontWeight: "400",
   color: "#646464",
-  marginTop: "12px",
-  marginLeft: "-200px",
+  // marginTop: "12px",
+  // marginLeft: "-200px",
 });
 
-const StylP = styled("P")({
+const StylP = styled("p")({
   fontSize: "16px",
   fontWeight: "400",
   color: "#646464",
-  marginTop: "45px",
-  marginLeft: "-55px",
+  // marginTop: "45px",
+  // marginLeft: "-55px",
 });
 const StyleP = styled("p")({
   fontSize: "18px",
   fontWeight: "500",
   color: "#383838",
-  marginTop: "10px",
-  marginLeft: "-150px",
+  // marginTop: "10px",
+  // marginLeft: "-150px",
 });
 
 const Styledh1 = styled("h1")({
@@ -82,3 +89,27 @@ const Styledh1 = styled("h1")({
   color: "#363636",
   fontWeight: "500",
 });
+// import { Card, CardContent, Typography, Avatar } from "@mui/material";
+// import Grid from "@mui/material/Grid";
+
+// export const Profile = ({ name, contact }) => {
+//   return (
+//     <Card sx={{ minWidth: 275, margin: "16px" }}>
+//       <CardContent>
+//         <Grid container alignItems="center">
+//           <Grid item>
+//             <Avatar sx={{ bgcolor: "blue", marginRight: "16px" }}>
+//               {name[0]}
+//             </Avatar>
+//           </Grid>
+//           <Grid item>
+//             <Typography variant="h6">{name}</Typography>
+//             <Typography variant="body2" color="text.secondary">
+//               Contact: {contact}
+//             </Typography>
+//           </Grid>
+//         </Grid>
+//       </CardContent>
+//     </Card>
+//   );
+// };
