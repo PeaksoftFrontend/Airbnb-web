9d944ae31526f9cdf3d6588806e4d54220cb072b
Munaraaz <munmk@bk.ru>
2025-01-07 21:05:43 +0300

    User/region

--- src/components/user/Regions.jsx
+++ src/components/user/Regions.jsx
@@ -1,13 +1,13 @@
 import { Box, Container, styled, Typography } from "@mui/material";
 
 import Batken from "../../assets/image/batken-img.png";
-// import Osh from "../../assets/image/osh-img.png";
+import Osh from "../../assets/image/osh-img.png";
 import Naryn from "../../assets/image/naryn-img.png";
 import Jalalabad from "../../assets/image/jalalabad-img.png";
-// import Talas from "../../assets/image/talas-img.png";
-// import Issykkol from "../../assets/image/issykkol-img.png";
+import Talas from "../../assets/image/talas-img.png";
+import Issykkol from "../../assets/image/issykkol-img.png";
 import Chui from "../../assets/image/chui-img.png";
-// import Bishkek from "../../assets/image/bishkek-img.png";
+import Bishkek from "../../assets/image/bishkek-img.png";
 
 export const Regions = () => {
   return (
@@ -19,28 +19,52 @@ export const Regions = () => {
           everything for a great vacation.
         </StyledH6>
       </Box>
-      <StyledBox>
-        <StyledDivs>
-          <img src={Chui} alt="" />
-          <StyledText variant="h6">CHUI</StyledText>
-        </StyledDivs>
-        <StyledDiv>
-          <StyleDiv>
-            <StyledDivs>
-              <img src={Batken} alt="" />
-              <StyledTex>BATKEN</StyledTex>
-            </StyledDivs>
-            <StyledDivs>
-              <img src={Jalalabad} alt="" />
-              <StyledTex>JALALABAT</StyledTex>
-            </StyledDivs>
-          </StyleDiv>
+      <StyleBoxContainer>
+        <StyledBox>
           <StyledDivs>
-            <img src={Naryn} alt="" />
-            <StyleNaryn>NARYN</StyleNaryn>
+            <img src={Chui} alt="" />
+            <StyledText variant="h6">CHUI</StyledText>
           </StyledDivs>
-        </StyledDiv>
-      </StyledBox>
+          <StyledDiv>
+            <StyleDiv>
+              <StyledDivs>
+                <img src={Batken} alt="" />
+                <StyledTex>BATKEN</StyledTex>
+              </StyledDivs>
+              <StyledDivs>
+                <img src={Jalalabad} alt="" />
+                <StyledTex>JALALABAT</StyledTex>
+              </StyledDivs>
+            </StyleDiv>
+            <StyledDivs>
+              <img src={Naryn} alt="" />
+              <StyleNaryn>NARYN</StyleNaryn>
+            </StyledDivs>
+          </StyledDiv>
+        </StyledBox>
+        <StylBox>
+          <StyledDiv>
+            <StylesDiv>
+              <StyledDivs>
+                <img src={Issykkol} alt="" />
+                <StyledTex>ISSYK-KUL</StyledTex>
+              </StyledDivs>
+              <StyledDivs>
+                <img src={Talas} alt="" />
+                <StyledTex>TALAS</StyledTex>
+              </StyledDivs>
+            </StylesDiv>
+            <StyledDivs>
+              <img src={Bishkek} alt="" />
+              <StyledTex>BISHKEK</StyledTex>
+            </StyledDivs>
+          </StyledDiv>
+          <StyledDivs>
+            <img src={Osh} alt="" />
+            <StyledText>OSH</StyledText>
+          </StyledDivs>
+        </StylBox>
+      </StyleBoxContainer>
     </StyledContainer>
   );
 };
@@ -51,6 +75,11 @@ const StyledContainer = styled(Container)({
   gap: "60px",
 });
 
+const StyleBoxContainer = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+});
 const StyledH4 = styled(Typography)({});
 
 const StyledH6 = styled(Typography)({});
@@ -68,9 +97,7 @@ const StyledDiv = styled("div")({
   flexDirection: "column",
   gap: "15.5px",
 });
-// const StyledChuiDiv = styled("div")({
-//   position: "relative",
-// });
+
 const StyleDiv = styled("div")({
   display: "flex",
   flexDirection: "row",
@@ -100,4 +127,18 @@ const StyleNaryn = styled(Typography)({
   top: "260px",
   left: "20px",
   color: "white",
+});
+
+const StylBox = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignContent: "center",
+  gap: "20px",
+  position: "relative",
+});
+
+const StylesDiv = styled("div")({
+  display: "flex",
+  flexDirection: "row",
+  gap: "20px",
 });
