cb99a5147ca68bb6953a39c0988d59bf9d5f1612
Aitula-svg <aitu.ru00@gmail.com>
2025-01-22 20:01:13 +0300

    infavorites

--- src/routes/user/UserRoutes.jsx
+++ src/routes/user/UserRoutes.jsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom";
 import { InnerOfHotel } from "../../pages/InnerOfHotel";
 import { PrivateRoute } from "../PrivateRoute";
 import { PATHS } from "../../utils/constants/paths";
+import { InFavorites } from "../../components/user/InFavorites";
 
 export const UserRoutes = () => {
   return [
@@ -9,17 +10,17 @@ export const UserRoutes = () => {
       path: PATHS.USER.ROOT,
       element: (
         <PrivateRoute
-          Component={<Navigate to={PATHS.USER.REGIONS} />}
+          Component={<Navigate to={PATHS.USER.INFAVORITES} />}
           isAuthorized={true}
           fallBackPath={PATHS.USER.ROOT}
         />
       ),
     },
     {
-      path: PATHS.USER.INNER_HOTEL_OF_REGIONS,
+      path: PATHS.USER.INFAVORITES,
       element: (
         <PrivateRoute
-          Component={<InnerOfHotel />}
+          Component={<InFavorites />}
           isAuthorized={true}
           fallBackPath={PATHS.USER.ROOT}
         />
