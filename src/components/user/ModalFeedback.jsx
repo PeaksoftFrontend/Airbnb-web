4ae88573284ce3a7cecaf6f0e4e982ac0f92563f
Nooruz <abdykalykovnooruz638@gmail.com>
2025-01-23 11:12:59 +0600

    changed feedback

--- src/components/user/ModalFeedback.jsx
+++ src/components/user/ModalFeedback.jsx
@@ -13,10 +13,10 @@ import { Icons } from "../../assets";
 import { useDropzone } from "react-dropzone";
 import { Textarea } from "@mui/joy";
 
-export const ModalFeedback = () => {
-  const [open, setOpen] = useState(false);
+export const ModalFeedback = ({ open = true, onClose }) => {
   const [rating, setRating] = useState(0);
   const [files, setFiles] = useState([]);
+
   const onDrop = (acceptedFiles) => {
     if (files.length + acceptedFiles.length <= 4) {
       setFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
@@ -48,21 +48,15 @@ export const ModalFeedback = () => {
     setFiles((prevFiles) => prevFiles.filter((file) => file !== fileToRemove));
   };
 
-  const handleOpenClick = () => {
-    setOpen(!open);
-  };
   return (
-    <Box>
-      <StyledButton variant="outlined" onClick={handleOpenClick}>
-        request to book
-      </StyledButton>
-      <Modal open={open} onClose={handleOpenClick}>
-        <StyledModal>
-          <StyledTitle>Leave feedback</StyledTitle>
-          <StyledContainer>
-            <Box>
-              <input {...getInputProps()} />
-              <StyledTextPhoto>
+    <Modal open={open} onClose={onClose}>
+      <StyledModal>
+        <StyledTitle>Leave feedback</StyledTitle>
+        <StyledContainer>
+          <Box>
+            <input {...getInputProps()} />
+            <StyledTextPhoto>
+              {files.length === 4 ? null : (
                 <StyledDivIcons>
                   <Tooltip title="Загрузить файл">
                     <IconButton
@@ -76,55 +70,54 @@ export const ModalFeedback = () => {
                     </IconButton>
                   </Tooltip>
                 </StyledDivIcons>
-                <StyledDivText>
-                  <StyledFirst variant="h7">
-                    Add photos to the review
-                  </StyledFirst>
-                  <StyledSecond>
-                    it will become more noticeable and even more useful. You can
-                    upload up to 4 photos.
-                  </StyledSecond>
-                  <StyledBoxImg>
-                    {files.map((file) => (
-                      <Box key={file.name}>
-                        <StyledImage
-                          src={URL.createObjectURL(file)}
-                          alt={`Uploaded file ${file.name}`}
-                        />
-                        <Cancellation onClick={() => removeFile(file)} />
-                      </Box>
-                    ))}
-                  </StyledBoxImg>
-                </StyledDivText>
-              </StyledTextPhoto>
-            </Box>
-            <SecondBox>
-              <Styledtext variant="h7">Rate</Styledtext>
-              <StyledRating
-                name="simple-controlled"
-                value={rating}
-                onChange={(event, newValue) => {
-                  setRating(newValue);
-                }}
-                precision={0.5}
-                emptyIcon={<StyledStarIcon />}
-              />
-            </SecondBox>
-            <SecondBox>
-              <Styledtext variant="h7">Feedback</Styledtext>
-              <StyledTextarea
-                maxRows={3}
-                placeholder="Share your impressions about this place"
-              />
-            </SecondBox>
-            <BoxButton>
-              <WhiteButton variant="white">cancel</WhiteButton>
-              <StyledButto variant="outlined">Public</StyledButto>
-            </BoxButton>
-          </StyledContainer>
-        </StyledModal>
-      </Modal>
-    </Box>
+              )}
+
+              <StyledDivText>
+                <StyledFirst variant="h6">Add photos to the review</StyledFirst>
+                <StyledSecond variant="inherit">
+                  it will become more noticeable and even more useful. You can
+                  upload up to 4 photos.
+                </StyledSecond>
+                <StyledBoxImg>
+                  {files.map((file) => (
+                    <Box key={file.name}>
+                      <StyledImage
+                        src={URL.createObjectURL(file)}
+                        alt={`Uploaded file ${file.name}`}
+                      />
+                      <Cancellation onClick={() => removeFile(file)} />
+                    </Box>
+                  ))}
+                </StyledBoxImg>
+              </StyledDivText>
+            </StyledTextPhoto>
+          </Box>
+          <SecondBox>
+            <Styledtext variant="h7">Rate</Styledtext>
+            <StyledRating
+              name="simple-controlled"
+              value={rating}
+              onChange={(event, newValue) => {
+                setRating(newValue);
+              }}
+              precision={0.5}
+              emptyIcon={<StyledStarIcon />}
+            />
+          </SecondBox>
+          <SecondBox>
+            <Styledtext variant="h7">Feedback</Styledtext>
+            <StyledTextarea
+              maxRows={3}
+              placeholder="Share your impressions about this place"
+            />
+          </SecondBox>
+          <BoxButton>
+            <WhiteButton variant="white">cancel</WhiteButton>
+            <StyledButto variant="outlined">Public</StyledButto>
+          </BoxButton>
+        </StyledContainer>
+      </StyledModal>
+    </Modal>
   );
 };
 const StyledTextarea = styled(Textarea)({
@@ -142,26 +135,32 @@ const StyledTextarea = styled(Textarea)({
     border: "1px solid #828282",
   },
 });
+
 const WhiteButton = styled(Button)({
   borderRadius: "none",
   width: "150px",
   height: "33px",
   boxShadow: "none",
 });
+
 const BoxButton = styled(Box)({
   display: "flex",
   gap: "8px",
   justifyContent: "end",
 });
+
 const StyledButto = styled(Button)({ width: "196px", height: "37px" });
+
 const SecondBox = styled(Box)({
   display: "flex",
   flexDirection: "column",
   gap: "12px",
 });
+
 const StyledStarIcon = styled(Icons.Star)`
   font-size: 30px;
 `;
+
 const StyledRating = styled(Rating)(() => ({
   "& .MuiRating-icon": {
     fontSize: "30px",
@@ -177,19 +176,21 @@ const StyledRating = styled(Rating)(() => ({
       color: "#F7D212",
     },
 }));
-const StyledButton = styled(Button)({ width: "423px", height: "37px" });
+
 const StyledModal = styled(Box)({
   width: "720px",
-  height: "463px",
+  padding: "25px",
   display: "flex",
   alignItems: "center",
   flexDirection: "column",
 });
+
 const StyledContainer = styled(Box)({
   display: "flex",
   gap: "22px",
   flexDirection: "column",
 });
+
 const StyledTitle = styled(Typography)({
   fontSize: "18px",
   fontWeight: "500",
@@ -223,7 +224,11 @@ const StyledDivText = styled("div")({
   gap: "8px",
 });
 
-const StyledFirst = styled(Typography)({ color: "#266BD3", fontWeight: "500" });
+const StyledFirst = styled(Typography)({
+  color: "#266BD3",
+  fontWeight: "500",
+  fontSize: "16px",
+});
 const StyledSecond = styled(Typography)({
   color: "#828282",
   fontSize: "14px",
@@ -235,7 +240,9 @@ const StyledImage = styled("img")({
   height: "40px",
   objectFit: "cover",
 });
+
 const Cancellation = styled(Icons.Cancellation)({ cursor: "pointer" });
+
 const StyledBoxImg = styled(Box)({
   display: "flex",
   flexDirection: "row",
