f6d3c1350380be0e1b227f50f63245ff571f8dae
Aitula-svg <aitu.ru00@gmail.com>
2025-02-19 19:56:48 +0300

    baseQuery

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,6 @@ import svgr from "vite-plugin-svgr";
 export const viteConfig = defineConfig({
   plugins: [react(), svgr()],
 });
+
+// Убедитесь, что Vite получает правильный объект
+export default viteConfig;
