01f461e6510d77f869124cb54558e719849e6552
Erzhan1212 <moldalieve-1296@mail.ru>
2025-03-11 20:50:39 +0300

    comment

--- src/layout/user/UserLayout.jsx
+++ src/layout/user/UserLayout.jsx
@@ -1,7 +1,6 @@
-import { FavoritePage } from "../../pages/user/FavoritePage";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
-import { useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export const UserLayout = () => {
   const { pathname } = useLocation();
@@ -10,8 +9,7 @@ export const UserLayout = () => {
     <div>
       {pathname === "/" ? null : <Header />}
       <main>
-        {/* <Outlet /> */}
-        <FavoritePage />
+        <Outlet />
       </main>
       <Footer />
     </div>
