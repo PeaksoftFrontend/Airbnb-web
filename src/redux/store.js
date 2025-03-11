506b5478104344bffb75e4a695af13dd018d5424
Aitula-svg <aitu.ru00@gmail.com>
2025-03-11 19:25:07 +0300

    resolve conflict

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,32 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { usersApi } from "./api/users.service";
 import { authApi } from "./api/auth.servers";
-<<<<<<< HEAD
 import { authSlice } from "./slices/authSlie";
 import { submitAdApi } from "./api/submitAdd.service";
-=======
-import { applicationApi } from "./api/application.service";
->>>>>>> origin
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
-<<<<<<< HEAD
     [submitAdApi.reducerPath]: submitAdApi.reducer,
-=======
-    [applicationApi.reducerPath]: applicationApi.reducer,
->>>>>>> origin
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       usersApi.middleware,
       authApi.middleware,
-<<<<<<< HEAD
       submitAdApi.middleware
-=======
-      applicationApi.middleware
->>>>>>> origin
     ),
 });
