881060080a497212fba446cbb2a7db02ad7985a9
Nooruz <abdykalykovnooruz638@gmail.com>
2025-03-13 12:19:18 +0600

    save

--- src/components/user/welcome-section/HeaderModal.jsx
+++ src/components/user/welcome-section/HeaderModal.jsx
@@ -103,7 +103,11 @@ export const HeaderModal = () => {
   };
 
   const handleNavigate = () => {
-    navigate(`${PATHS.USER.PUBLISH}`);
+    if (role === "USER") {
+      navigate(`${PATHS.USER.PUBLISH}`);
+    } else {
+      setModalOpen(true);
+    }
   };
 
   return (
@@ -239,6 +243,7 @@ const StyledLink = styled(Box)({
   fontSize: "18px",
   fontWeight: "500",
   color: "#FFF",
+  cursor: "pointer",
 });
 
 const StyledButton = styled(Button)({
